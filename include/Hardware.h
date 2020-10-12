5c8b621235e8f823249ed61a1bda7d038900869f
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-12 18:07:31 +0200

    Add more pins.

--- include/Hardware.h
+++ include/Hardware.h
@@ -28,12 +28,44 @@
 namespace hardware
 {
     /**
-     * Defines the
+     * Defines the pin for the first button.
      */
     constexpr uint8_t BUTTON_1_PIN = 0;
+
+    /**
+     * Defines the pin for the second button.
+     */
     constexpr uint8_t BUTTON_2_PIN = 1;
+
+    /**
+     * Defines the pin for the third button.
+     */
     constexpr uint8_t BUTTON_3_PIN = 2;
+
+    /**
+     * Defines the pin for the fourth button.
+     */
     constexpr uint8_t BUTTON_4_PIN = 3;
+
+    /**
+     * Defines the pin for the potentiometer.
+     */
+    constexpr uint8_t POTENTIOMETER_PIN = 4;
+
+    /**
+     * Defines the pin for the servo.
+     */
+    constexpr uint8_t SERVO_PIN = 5;
+
+    /**
+     * Defines the pin for the LCD data.
+     */
+    constexpr uint8_t LCD_DATA_PIN = A4;
+
+    /**
+     * Defines the pin for the LCD clock.
+     */
+    constexpr uint8_t LCD_CLOCK_PIN = A5;
 }
 
 #endif //DISTILLERY_CONTROLLER_HARDWARE_H
