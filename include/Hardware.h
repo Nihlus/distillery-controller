70d1afab62e5275dabc8913feb2ef6a2fdd4e6df
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-12 21:28:45 +0200

    Fix LCD hardware settings.

--- include/Hardware.h
+++ include/Hardware.h
@@ -7,16 +7,16 @@
 //  Copyright (c) 2020 Jarl Gullberg
 //
 //  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
+//  it under the terms of the GNU Affero General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
+//  GNU Affero General Public License for more details.
 //
-//  You should have received a copy of the GNU Lesser General Public License
+//  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
@@ -80,17 +80,17 @@ namespace hardware
     /**
      * Defines the I²C address of the LCD.
      */
-    constexpr uint8_t LCD_I2C_ADDRESS = 0x78;
+    constexpr uint8_t LCD_I2C_ADDRESS = 0x3C;
 
     /**
      * Defines the height of the LCD screen.
      */
-    constexpr pixels_t LCD_HEIGHT = 128;
+    constexpr pixels_t LCD_HEIGHT = 64;
 
     /**
      * Defines the height of the LCD screen.
      */
-    constexpr pixels_t LCD_WIDTH = 64;
+    constexpr pixels_t LCD_WIDTH = 128;
 
     /**
      * Defines the lower bound of the pulse width for the servo.
