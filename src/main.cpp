da42627cad5dff8875ac2faa371393ebf28fa723
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-12 21:28:52 +0200

    Initial implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -21,17 +21,85 @@
 //
 
 #include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
 #include "Hardware.h"
+#include "Settings.h"
+#include "Button.h"
+
+#define lengthof(a) sizeof(a) / sizeof(a[0])
+
+Button buttons[] =
+{
+    Button(hardware::BUTTON_1_PIN),
+    Button(hardware::BUTTON_2_PIN),
+    Button(hardware::BUTTON_3_PIN),
+    Button(hardware::BUTTON_4_PIN)
+};
+
+uint8_t buttonAngles[] =
+{
+    settings::BUTTON_1_ANGLE,
+    settings::BUTTON_2_ANGLE,
+    settings::BUTTON_3_ANGLE,
+    settings::BUTTON_4_ANGLE,
+};
+
+uint8_t servoAngle = 0;
+
+Servo servo;
+Adafruit_SSD1306 lcd;
 
 void setup()
 {
-    pinMode(hardware::BUTTON_1_PIN, INPUT_PULLUP);
-    pinMode(hardware::BUTTON_2_PIN, INPUT_PULLUP);
-    pinMode(hardware::BUTTON_3_PIN, INPUT_PULLUP);
-    pinMode(hardware::BUTTON_4_PIN, INPUT_PULLUP);
+    for (auto& button : buttons)
+    {
+        button.initialize();
+    }
+
+    pinMode(hardware::POTENTIOMETER_PIN, INPUT);
+    pinMode(hardware::SERVO_PIN, OUTPUT);
+
+    servo.attach
+    (
+        hardware::SERVO_PIN,
+        hardware::SERVO_MIN_PULSE_WIDTH,
+        hardware::SERVO_MAX_PULSE_WIDTH
+    );
+
+    // Start at 0 degrees
+    servo.write(servoAngle);
+
+    // Initialize the LCD
+    lcd = Adafruit_SSD1306(hardware::LCD_WIDTH, hardware::LCD_HEIGHT, &Wire, -1);
+    lcd.begin(SSD1306_SWITCHCAPVCC, hardware::LCD_I2C_ADDRESS);
+
+    lcd.clearDisplay();
+    lcd.setTextColor(WHITE, BLACK);
 }
 
 void loop()
 {
+    lcd.clearDisplay();
+    for (auto i = 0; i < lengthof(buttons); ++i)
+    {
+        auto& button = buttons[i];
+
+        auto circleX = i * 12 + 8;
+        auto circleY = 24;
+
+        lcd.drawCircle(circleX, circleY, 4, WHITE);
+
+        if (button.isPressed())
+        {
+            lcd.fillCircle(circleX, circleY, 2, WHITE);
+            servoAngle = buttonAngles[i];
+        }
+    }
+
+    servo.write(servoAngle);
+
+    lcd.display();
 }
